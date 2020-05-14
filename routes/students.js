dfb333dc2756ded0f1a9b357829a518c07f55899
최영은 <choeyeong-eun@choeyeong-eun-ui-MacBookAir.local>
2020-05-14 17:46:41 +0900

    graph menu added

--- routes/students.js
+++ routes/students.js
@@ -51,4 +51,31 @@ router.put('/:id', function(req, res){
     });
   });
 
+  // Students - Graph 
+router.get('/graph', function (req, res) {
+  Student.find({}, function (err, students) {
+      if (err) return res.json(err)
+      Scan.find({}, function (err, scans) {
+        if (err) return res.json(err)
+        var counts_O = 0
+        var counts_X = 0
+        for(var s in students) {
+          for(var i in scans){
+            // UTC로 저장된 몽고디비 시간을 한국 시간대(-9시간)로 변경
+            var scanTimeInMilliSec = new Date(scans[i].time).getTime()-32400000 
+    
+            // 스캔된 데이터의 맥주소와 유저의 맥주소가 서로 일치하고 스캔된 데이터의 시간이 현재 시간에서 3분 안의 데이터이면 "O"를 return    
+            if(scans[i].address == students[s].mac && scanTimeInMilliSec > new Date().getTime() - 300000){
+                counts_O++
+                break
+            } 
+          }
+        }
+        
+      counts_X = students.length - counts_O
+      res.render('students/graph', { counts_O: counts_O, counts_X: counts_X});
+      })
+  });
+});
+
   module.exports = router;
