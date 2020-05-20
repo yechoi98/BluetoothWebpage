b7a90c92f6b5d4f392e7878c597f6eb6411d1d2c
최영은 <lemonjuice123@naver.com>
2020-05-20 16:46:15 +0900

    result collection 삽입/수정/삭제

--- routes/students.js
+++ routes/students.js
@@ -1,54 +1,68 @@
 var express = require('express');
 var router = express.Router();
-var Student = require('../models/Student'); 
-var Scan = require('../models/Scan'); 
+var Student = require('../models/Student');
+var Scan = require('../models/Scan');
+var Result = require('../models/Result');
 
-// Students - Index 
+// Students - Index
 router.get('/', function (req, res) {
     Student.find({}, function (err, students) {
         if (err) return res.json(err);
         Scan.find({}, function (err, scans) {
-          if (err) return res.json(err);
-          res.render('students/index', { students: students, scans: scans});
-        })
+            if (err) return res.json(err);
+            res.render('students/index', { students: students, scans: scans });
+        });
     });
 });
 
-// Students - New 
+// Students - New
 router.get('/new', function (req, res) {
     res.render('students/new');
 });
 
-// Students - create 
+// Students - create
 router.post('/', function (req, res) {
     Student.create(req.body, function (err, student) {
         if (err) return res.json(err);
         res.redirect('/students');
     });
-});
-
-// Students - edit 
-router.get('/:id/edit', function(req, res){
-    Student.findOne({_id:req.params.id}, function(err, student){
-      if(err) return res.json(err);
-      res.render('students/edit', {student:student});
+    Result.create({ mac: req.body.mac }, function (err, result) {
+        if (err) return res.json(err);
     });
 });
 
-// Students - update 
-router.put('/:id', function(req, res){
-    Student.findOneAndUpdate({_id:req.params.id}, req.body, function(err, student){
-      if(err) return res.json(err);
-      res.redirect('/students');
+// Students - edit
+router.get('/:id/edit', function (req, res) {
+    Student.findOne({ _id: req.params.id }, function (err, student) {
+        if (err) return res.json(err);
+        res.render('students/edit', { student: student });
     });
 });
 
-  // Students - destroy
-  router.delete('/:id', function(req, res){
-    Student.deleteOne({_id:req.params.id}, function(err){
-      if(err) return res.json(err);
-      res.redirect('/students');
+// Students - update
+router.put('/:mac', function (req, res) {
+    Student.findOneAndUpdate({ mac: req.params.mac }, req.body, function (err, student) {
+      console.log(req.params.mac)
+        if (err) return res.json(err);
+        res.redirect('/students');
     });
-  });
+    Result.findOneAndUpdate({mac : req.params.mac}, {mac : req.body.mac}, function (err, result) {
+      if(err) return res.json(err);
+    })
+});
 
-  module.exports = router;
+// Students - destroy
+router.delete('/:mac', function (req, res) {
+    Student.deleteOne({ mac: req.params.mac }, function (err) {
+
+      console.log(req.params.mac)
+        if (err) return res.json(err);
+        res.redirect('/students');
+    });
+
+    Result.deleteOne({mac : req.params.mac}, function (err) {
+      if(err) return res.json(err);
+    })
+});
+
+module.exports = router;
