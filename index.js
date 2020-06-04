38c30fe0d91b15cb37609722a91a7e6e173c38e6
최영은 <lemonjuice123@naver.com>
2020-06-04 18:48:55 +0900

    3000port

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/', require('./routes/home'));
 app.use('/students', require('./routes/students'));
 
 // Port setting
-var port = 3200;
+var port = 3000;
 app.listen(port, function () {
     console.log('server on! http://localhost:' + port);
 });
