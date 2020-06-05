99d9c91ceac0ec18032219d22f2daf8dc25f5cd6
최영은 <lemonjuice123@naver.com>
2020-06-05 19:31:16 +0900

    graph

--- routes/students.js
+++ routes/students.js
@@ -7,7 +7,6 @@ var f = require('../public/js/functions')
 
 // Students - Index
 router.get('/', function (req, res) {
-    
     Student.find({}, function (err, students) {
         if (err) return res.json(err);
         Result.find({}, function (err, results) {
@@ -20,6 +19,7 @@ router.get('/', function (req, res) {
 // Students - Graph
 router.get('/graph', function(req, res){
     Student.find({}, function(err, students){
+        if (err) res.json(err)
         Result.find({}, function(err, results) {
             if (err) res.json(err)
             res.render('students/graph', { o: f.getCountsO(results, students), x: f.getCountsX(results, students)});
@@ -36,11 +36,11 @@ router.get('/new', function (req, res) {
 router.post('/', function (req, res) {
     Student.create(req.body, function (err, student) {
         if (err) return res.json(err);
+        Result.create({ mac: req.body.mac }, function (err, result) {
+            if (err) return res.json(err);
+            res.redirect('/students');
+        });
     });
-    Result.create({ mac: req.body.mac }, function (err, result) {
-        if (err) return res.json(err);
-    });
-    res.redirect('/students');
 });
 
 // Students - edit
@@ -54,27 +54,23 @@ router.get('/:id/edit', function (req, res) {
 // Students - update
 router.put('/:mac', function (req, res) {
     Student.findOneAndUpdate({ mac: req.params.mac }, req.body, function (err, student) {
-      console.log(req.params.mac)
         if (err) return res.json(err);
+        Result.findOneAndUpdate({mac : req.params.mac}, {mac : req.body.mac}, function (err, result) {
+            if(err) return res.json(err);
+            res.redirect('/students');
+          })
     });
-    Result.findOneAndUpdate({mac : req.params.mac}, {mac : req.body.mac}, function (err, result) {
-      if(err) return res.json(err);
-    })
-    res.redirect('/students');
 });
 
 // Students - destroy
 router.delete('/:mac', function (req, res) {
     Student.deleteOne({ mac: req.params.mac }, function (err) {
-      console.log(req.params.mac)
         if (err) return res.json(err);
+        Result.deleteOne({mac : req.params.mac}, function (err) {
+            if(err) return res.json(err);
+            res.redirect('/students');
+        })
     });
-
-    Result.deleteOne({mac : req.params.mac}, function (err) {
-      if(err) return res.json(err);
-    })
-
-    res.redirect('/students');
 });
 
 
