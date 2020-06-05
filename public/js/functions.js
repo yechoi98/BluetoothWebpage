9e6804d8224d1c90040b5ac73d3eee816f5e1f8b
최영은 <lemonjuice123@naver.com>
2020-06-05 19:24:43 +0900

    function.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,26 +3,23 @@ getCountsO = function(results, students) {
     var o = 0;
     if(results.length == 0 || students.length == 0) return o;
     for (var i in results) {
-        var scanTimeInMilliSec = new Date(results[i].time).getTime() - 32400000;
-            if (results[i].time != null && scanTimeInMilliSec >= new Date().getTime() - 300000) {
-                o++;
+        for (var j in students) {
+            if(students[j].mac == results[i].mac) {
+                var scanTimeInMilliSec = new Date(results[i].time).getTime() - 32400000;
+                if (results[i].time != null && scanTimeInMilliSec >= new Date().getTime() - 300000) {
+                    o++;
+                }
             }
+        }
     }
 
     return o;
 }
 
 getCountsX = function(results, students) {
-    var x = 0;
     if(results.length == 0 || students.length == 0) return students.length;
-    for (var i in results) {
-        var scanTimeInMilliSec = new Date(results[i].time).getTime() - 32400000;
-        if (results[i].time != null && scanTimeInMilliSec <= new Date().getTime() - 300000) {
-            x++;
-        }
-    }
-
-    return x;
+ 
+    return students.length - getCountsO(results, students);
 }
 
 module.exports = {
